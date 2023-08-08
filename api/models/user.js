355b9eb7be7b97751bd097352cce08103261fc60
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-08-08 10:12:49 +0100

    bycrpt password on sign-up

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   surname: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true, index: true },
   password: { type: String, required: true },
+});
+
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
