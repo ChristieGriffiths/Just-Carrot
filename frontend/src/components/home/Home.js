59b267b6d68a512c89ffd166173221b820d6ed48
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-08-11 10:56:25 +0100

    how it works appears uglyly

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import './Home.css'
-import logo from '../../assets/logo.png';
-import homePageImage from '../../assets/homePageImage.png';
-import aboutUs from '../../assets/aboutUs.png';
 import { Link } from "react-router-dom";
 
+import logo from '../../assets/logo.png';
+import homePageImage from '../../assets/homePageImage.png';
+import howItWorks from '../../assets/howItWorks.png';
+import aboutUs from '../../assets/aboutUs.png';
+
 const Home = () => {
+  const handleAboutUsClick = () => {
+    console.log('clicked')
+    const aboutUsImageElement = document.getElementById('about-us-container');
+
+    if (aboutUsImageElement) {
+      aboutUsImageElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.log('Element not found.');
+    }
+  };
+
   return (
     <div className="home-container">
         <h1 className="how-it-works">how it works</h1>
-        <h1 className="about-us">about us</h1>
+        <h1 className="about-us" onClick={handleAboutUsClick}>about us</h1>
         <h1 className="sign-up-in"><Link to="/signup">sign up</Link>/<Link to="/login">sign in</Link></h1>
       <header className="top-banner">
         <img src={logo} alt="Logo" />
       </header>
       <div className="content-container">
-        <div className="logo-container">
-          <img src={homePageImage} alt="Logo" />
+        <div className="home-page-container">
+          <img src={homePageImage} alt="img" />
         </div>
-        <div className="logo-container">
-          <img src={aboutUs} alt="Logo" />
+        <div className="how-it-works-image">
+          <img src={howItWorks} alt="img" />
+        </div>
+        <div className="about-us-container" id="about-us-container">
+          <img src={aboutUs} alt="img" />
         </div>
       </div>
     </div>
