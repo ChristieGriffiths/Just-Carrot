6de089ee626ccf4a54f690145aafa428a32cd780
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-09-04 08:49:11 +0100

    create logic that challenge dissapears once time has elapsed

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -2,15 +2,30 @@ const Post = require("../models/post");
 const TokenGenerator = require("../models/token_generator");
 
 const PostsController = {
-  Index: (req, res) => {
-    Post.find(async (err, posts) => {
-      if (err) {
-        return res.status(500).json({ message: err.message });
-      }
+  Index: async (req, res) => {
+    try {
+      let posts = await Post.find();
+
+      // Loop through posts and update those with elapsed times and null 'completed' fields.
+      const updatedPosts = await Promise.all(posts.map(async (post) => {
+        const completeDateTime = new Date(`${post.completeDate}T${post.completeTime}`);
+        const currentDateTime = new Date();
+        
+        if (currentDateTime > completeDateTime && post.completed === null) {
+          post.completed = false;
+          await post.save();
+        }
+        
+        return post;
+      }));
+
       const token = await TokenGenerator.jsonwebtoken(req.user_id);
-      res.status(200).json({ posts: posts, token: token });
-    });
+      res.status(200).json({ posts: updatedPosts, token: token });
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
   },
+
   Create: (req, res) => {
     const post = new Post(req.body);
     post.save(async (err) => {
@@ -21,6 +36,7 @@ const PostsController = {
       res.status(201).json({ message: 'OK', token: token });
     });
   },
+
   Update: async (req, res) => {
     try {
       const id = req.params.id;
--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './Post.css';
 
+const formatDate = (isoDate) => {
+  const date = new Date(isoDate);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+};
+
 const Post = ({ post, token, onUpdate }) => {
   // Only proceed if the "completed" field is null
   if (post.completed !== null) {
@@ -22,13 +30,11 @@ const Post = ({ post, token, onUpdate }) => {
 
       if (response.status === 200) {
         console.log("Successfully updated");
-        let data = await response.json();
+        const data = await response.json();
         onUpdate(data);
       } else if (response.status === 400) {
-        // Handle bad request
         console.log("Bad request");
       } else {
-        // Handle other statuses
         console.log("Failed to update");
       }
 
@@ -37,11 +43,13 @@ const Post = ({ post, token, onUpdate }) => {
     }
   };
 
+  const formattedDate = formatDate(post.completeDate);
+
   return (
     <div className="apple-style-container">
       <article data-cy="post" key={post.challenge}>
         Challenge: {post.challenge} <br />
-        Complete by: {post.completeDate} <br />
+        Complete by: {formattedDate} {post.completeTime} <br />
         Incentive: £ {post.incentiveAmount} <br />
         Charity: {post.chosenCharity} <br />
         <button onClick={handleConfirm}>Confirm</button>
