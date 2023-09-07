f5ca8134f3f2f9b6f8b6d2b57ea7916955d957b4
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-09-07 09:02:06 +0100

    set up backend as per video but no working atm

--- api/app.js
+++ api/app.js
@@ -17,8 +17,31 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(logger("dev"));
+app.use(cors());
+
+app.post("payment", cors(), async(req, res) => {
+  let {amount, id} = req.body 
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount, 
+      currency: "USD",
+      description: "Just Carrot",
+      payment_method: id,
+      confirm: true
+    })
+    console.log("Payment", payment)
+    res.json({
+      message: "Payment successfull",
+      success: true
+    })
+  } catch (error) {
+    console.log("Error", error)
+    res.json({
+      message: "Payment failed",
+      success: false
+    })
+  }
+} )
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Server is listening on port 4000")
