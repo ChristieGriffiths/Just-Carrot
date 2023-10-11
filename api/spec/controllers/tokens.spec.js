69598477ae6dfaf0ef2a4bc2d556690566c154a2
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-10-11 11:46:34 +0100

    fix token controller test

--- api/spec/controllers/tokens.spec.js
+++ api/spec/controllers/tokens.spec.js
@@ -1,34 +1,49 @@
+const mongoose = require("mongoose");
 const app = require("../../app");
 const request = require("supertest");
-require("../mongodb_helper");
 const User = require('../../models/user');
 
-describe("/tokens", () => {
-  beforeAll( () => {
-    const user = new User({ email: "test@test.com", password: "12345678" })
-    user.save()
-  });
+// Setup MongoDB connection for testing
+beforeAll(async () => {
+  const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/testDatabase";
+  await mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  afterAll( async () => {
-    await User.deleteMany({})
-  })
+  const user = new User({ 
+    firstName: 'test', 
+    surname: 'test', 
+    email: "test@test.com", 
+    password: "12345678" 
+  });
+  await user.save();
+});
+
+// Teardown MongoDB connection
+afterAll(async () => {
+  await User.deleteMany({});
+  await mongoose.connection.close();
+});
+
+// Test suite
+describe("/tokens", () => {
 
   test("a token is returned when creds are valid", async () => {
-    let response = await request(app)
-      .post("/tokens")
-      .send({email: "test@test.com", password: "12345678"})
-    expect(response.status).toEqual(201)
-    expect(response.body.token).not.toEqual(undefined)
-    expect(response.body.message).toEqual("OK")
-  })
+    const response = await request(app)
+      .post("/api/tokens")
+      .send({ email: "test@test.com", password: "12345678" });
 
+    expect(response.status).toEqual(201);
+    expect(response.body.token).not.toEqual(undefined);
+    expect(response.body.message).toEqual("OK");
+  });
 
   test("a token is not returned when creds are invalid", async () => {
-    let response = await request(app)
-      .post("/tokens")
-      .send({email: "test@test.com", password: "1234"})
-    expect(response.status).toEqual(401)
-    expect(response.body.token).toEqual(undefined)
-    expect(response.body.message).toEqual("auth error")
-  })
-})
+    const response = await request(app)
+      .post("/api/tokens")
+      .send({ email: "test@test.com", password: "wrong-password" });
+
+    expect(response.status).toEqual(401);
+    expect(response.body.token).toEqual(undefined);
+    expect(response.body.message).toEqual("auth error");
+  });
+
+});
