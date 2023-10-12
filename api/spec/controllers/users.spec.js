96923ffa767178be82ceb199e3ccb631bd024f97
Christie Griffiths <115458394+ChristieGriffiths@users.noreply.github.com>
2023-10-12 14:31:06 +0100

    fixed when password missing test

--- api/spec/controllers/users.spec.js
+++ api/spec/controllers/users.spec.js
@@ -19,20 +19,20 @@ describe("/users", () => {
     test("a user is created", async () => {
       await request(app)
         .post("/api/users")
-        .send({firstName: "test", surname: "test",email: "scarlett@email.com", password: "1234"})
+        .send({firstName: "test", surname: "test", email: "scarlett@email.com", password: "1234"})
       let users = await User.find()
       let newUser = users[users.length - 1]
       expect(newUser.email).toEqual("scarlett@email.com")
     })
-  // })
+  })
 
-  // describe("POST, when password is missing", () => {
-  //   test("response code is 400", async () => {
-  //     let response = await request(app)
-  //       .post("/users")
-  //       .send({email: "skye@email.com"})
-  //     expect(response.statusCode).toBe(400)
-  //   });
+  describe("POST, when password is missing", () => {
+    test("response code is 400", async () => {
+      let response = await request(app)
+        .post("/api/users")
+        .send({firstName: "test", surname: "test",email: "scarlett@email.com"})
+      expect(response.statusCode).toBe(400)
+    });
 
   //   test("does not create a user", async () => {
   //     await request(app)
